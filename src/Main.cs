b602baf63f52aff59bddb89f1fad63a7e28593f0
Madevil <meowmeowandy@gmail.com>
2020-11-22 23:57:49 +0800

    Got color working, and a toggle to switch on and off

--- src/Main.cs
+++ src/Main.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 using UnityEngine;
+using UniRx;
 
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 
 using KKAPI.Maker;
-using KKAPI.Utilities;
+using KKAPI.Maker.UI.Sidebar;
 
 namespace AccGotHigh
 {
@@ -18,92 +19,122 @@ namespace AccGotHigh
 	{
 		public const string Name = "AccGotHigh";
 		public const string GUID = "madevil.kk.AccGotHigh";
-		public const string Version = "1.0.0.0";
+		public const string Version = "1.1.0.0";
 
 		internal static new ManualLogSource Logger;
 		internal static MonoBehaviour Instance;
-		internal static Harmony HooksInstance;
+		internal static Harmony HooksInstance = null;
+
+		internal static ConfigEntry<bool> ConfigEnable { get; set; }
+		internal static ConfigEntry<int> ConfigRenderQueue { get; set; }
+		internal static ConfigEntry<Color> ConfigColor { get; set; }
+		internal static SidebarToggle SidebarToggleEnable;
 
 		internal static Material bonelyfans;
-		internal static List<Transform> Copies = new List<Transform>();
+		internal static List<Transform> EffectClones = new List<Transform>();
 		internal static ChaControl chaCtrl;
 
 		private void Start()
 		{
 			Logger = base.Logger;
 			Instance = this;
-			bonelyfans = LoadShader();
+
+			ConfigEnable = Config.Bind("General", "Enable", true);
+			ConfigEnable.SettingChanged += (sender, args) =>
+			{
+				if (MakerAPI.InsideMaker)
+				{
+					if (ConfigEnable.Value)
+						EnableHarmonyPatch();
+					else
+						DisableHarmonyPatch();
+				}
+			};
+			ConfigRenderQueue = Config.Bind("General", "Render Queue", 5000);
+			ConfigRenderQueue.SettingChanged += (sender, args) =>
+			{
+				bonelyfans.renderQueue = ConfigRenderQueue.Value;
+			};
+			ConfigColor = Config.Bind("General", "Effect Color", Color.cyan);
+			ConfigColor.SettingChanged += (sender, args) =>
+			{
+				bonelyfans.color = ConfigColor.Value;
+			};
+
+			bonelyfans = new Material(Shader.Find("Standard"));
+			bonelyfans.renderQueue = ConfigRenderQueue.Value;
+			bonelyfans.color = ConfigColor.Value;
 
 			MakerAPI.MakerBaseLoaded += (s, e) =>
 			{
-				HooksInstance = Harmony.CreateAndPatchAll(typeof(Hooks));
+				if (ConfigEnable.Value)
+					EnableHarmonyPatch();
 				chaCtrl = MakerAPI.GetCharacterControl();
 			};
-
+			MakerAPI.RegisterCustomSubCategories += (sender, e) =>
+			{
+				SidebarToggleEnable = e.AddSidebarControl(new SidebarToggle("AccGotHigh", ConfigEnable.Value, this));
+				SidebarToggleEnable.ValueChanged.Subscribe(value => ConfigEnable.Value = value);
+			};
 			MakerAPI.MakerExiting += (s, e) =>
 			{
-				HooksInstance.UnpatchAll(HooksInstance.Id);
-				HooksInstance = null;
+				DisableHarmonyPatch();
 				chaCtrl = null;
 			};
 		}
 
-		private static Material LoadShader()
+		internal static void EnableHarmonyPatch()
 		{
-			AssetBundle ab = null;
-			try
-			{
-				var res = ResourceUtils.GetEmbeddedResource("bonelyfans.unity3d") ?? throw new ArgumentNullException("GetEmbeddedResource");
-				ab = AssetBundle.LoadFromMemory(res) ?? throw new ArgumentNullException("LoadFromMemory");
-				var sha = ab.LoadAsset<Shader>("assets/bonelyfans 1.shader") ?? throw new ArgumentNullException("LoadAsset");
-				ab.Unload(false);
-				return new Material(sha);
-			}
-			catch (Exception)
-			{
-				if (ab != null) ab.Unload(true);
-				throw;
-			}
+			if (HooksInstance == null)
+				HooksInstance = Harmony.CreateAndPatchAll(typeof(Hooks));
 		}
 
-		internal static void AddEffect(List<GameObject> lalala)
+		internal static void DisableHarmonyPatch()
 		{
-			for (int i = 0; i < lalala.Count; i++)
+			if (HooksInstance != null)
 			{
-				if (lalala[i].name.StartsWith("AccGotHigh_"))
-					continue;
+				HooksInstance.UnpatchAll(HooksInstance.Id);
+				HooksInstance = null;
+			}
+			RemoveEffect();
+		}
 
-				if (GameObject.Find("AccGotHigh_" + lalala[i].gameObject.name) != null)
-					continue;
+		internal static void AddEffect(List<GameObject> groupie)
+		{
+			for (int i = 0; i < groupie.Count; i++)
+			{
+				if (groupie[i].name.StartsWith("AccGotHigh_")) continue;
+				if (GameObject.Find("AccGotHigh_" + groupie[i].gameObject.name) != null) continue;
 
-				Transform origin = lalala[i].transform;
+				Transform origin = groupie[i].transform;
 				Transform copy = Instantiate(origin, origin.parent, false);
 				copy.name = "AccGotHigh_" + origin.name;
 				copy.GetComponent<Renderer>().material = bonelyfans;
-				Copies.Add(copy);
+				EffectClones.Add(copy);
 			}
+		}
+
+		internal static void RemoveEffect()
+		{
+			for (int i = 0; i < EffectClones.Count; i++)
+				Destroy(EffectClones[i].gameObject);
+			EffectClones.Clear();
 		}
 
 		internal static void CtrlEffect(int slot, bool show)
 		{
-			if (slot > -1)
+			if (slot < 0) return;
+
+			ChaAccessoryComponent chaAccessory = chaCtrl.GetAccessory(slot);
+			if (chaAccessory == null) return;
+
+			if (show)
 			{
-				ChaAccessoryComponent chaAccessory = chaCtrl.GetAccessory(slot);
-				if (chaAccessory != null)
-				{
-					if (show)
-					{
-						AddEffect(chaAccessory.GetComponentsInChildren<MeshRenderer>().Select(x => x.gameObject).ToList());
-						AddEffect(chaAccessory.GetComponentsInChildren<SkinnedMeshRenderer>().Select(x => x.gameObject).ToList());
-					}
-					else
-					{
-						for (int i = 0; i < Copies.Count; i++)
-							Destroy(Copies[i].gameObject);
-						Copies.Clear();
-					}
-				}
+				AddEffect(chaAccessory.GetComponentsInChildren<MeshRenderer>().Select(x => x.gameObject).ToList());
+				AddEffect(chaAccessory.GetComponentsInChildren<SkinnedMeshRenderer>().Select(x => x.gameObject).ToList());
 			}
+			else
+				RemoveEffect();
 		}
 	}
 }
